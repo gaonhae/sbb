6cd535b7c3d685ea1c5d49f2ccc0b9d9716bad7b
gaonhae <codegaonhae@gmail.com>
2022-07-20 09:09:06 +0900

    feat : Apply anchor && Apply markdown

--- sbb/src/main/java/com/example/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/example/sbb/answer/AnswerController.java
@@ -41,8 +41,8 @@ public class AnswerController {
             model.addAttribute("question", question);	//questionDetail 템플릿이 question 객체를 필요로 하므로
             return "questionDetail";
 		}
-		this.answerService.create(question, answerForm.getContent(), siteUser);
-		return String.format("redirect:/question/detail/%s", id);
+		Answer answer = this.answerService.create(question, answerForm.getContent(), siteUser);
+		return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	}
 
 
@@ -69,7 +69,7 @@ public class AnswerController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
 		}
 		this.answerService.modify(answer, answerForm.getContent());
-		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+		return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	}
 
 
@@ -91,7 +91,7 @@ public class AnswerController {
 		Answer answer = this.answerService.getAnswer(id);
 		SiteUser siteUser = this.userService.getUser(principal.getName());
 		this.answerService.vote(answer, siteUser);
-		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+		return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	}
 
 
--- sbb/src/main/java/com/example/sbb/answer/AnswerService.java
+++ sbb/src/main/java/com/example/sbb/answer/AnswerService.java
@@ -17,13 +17,14 @@ public class AnswerService {
 	private final AnswerRepository answerRepository;
 
 	//답변 생성 메서드
-	public void create(Question question, String content, SiteUser author) {
+	public Answer create(Question question, String content, SiteUser author) {
 		Answer answer = new Answer();
 		answer.setContent(content);
 		answer.setCreateDate(LocalDateTime.now());
 		answer.setQuestion(question);
 		answer.setAuthor(author);
 		this.answerRepository.save(answer);
+		return answer;
 	}
 
 	public Answer getAnswer(Integer id) {
